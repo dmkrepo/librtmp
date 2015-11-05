f8ada9b904a4ce235ee8a26b2f1446a4d189451a
Dmitriy <dmitriy.ka256@gmail.com>
2015-11-05 17:41:00 +0300

    Visual Studio fixes

--- rtmpdump/rtmpdump.c
+++ rtmpdump/rtmpdump.c
@@ -284,7 +284,7 @@ GetLastKeyframe(FILE * file,	// output file [in]
 		int *initialFrameType,	// initial frame type (audio/video) [out]
 		uint32_t * nInitialFrameSize)	// length of initialFrame [out]
 {
-  const size_t bufferSize = 16;
+  enum{ bufferSize = 16 };
   char buffer[bufferSize];
   uint8_t dataType;
   int bAudioOnly;
